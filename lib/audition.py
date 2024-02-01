0c333528f98a8ad2a2d15d8d315b94aa9edd8670
Tim McCann <timothysmccann@gmail.com>
2024-02-01 16:59:07 -0500

    part done

--- lib/audition.py
+++ lib/audition.py
@@ -1,2 +1,41 @@
-class Audition:
+class Audition: #the actor/actress trying out for the character
+
+    all_auditions = []
+
+    def __init__(self, role, actor, location, phone):
+        self.actor = actor
+        self.location = location
+        self.phone = phone
+        self._hired = False
+        self._role = role
+
+    @property
+    def actor(self):
+        return self._actor
+    
+    @actor.setter
+    def actor(self, actor):
+        if  type(actor) == str and ((len(actor) > 3) and (len(actor) < 20)):
+            self._actor = actor
+        else:
+            raise Exception("Actor must be a string greater than 0 characters and less than 20 characters")
+        
+    @property
+    def role(self):
+        return self._role
+    
+    @role.setter
+    def role(self, role):
+        from role import Role
+        if isinstance(role, Role):
+            self._role = role
+            Audition.all_auditions.append(self)
+        else:
+            raise Exception("Role must be an instance of Role")
+        
+    def call_back(self):
+        self._hired = True
     pass
+
+
+print 
--- lib/role.py
+++ lib/role.py
@@ -1,5 +1,31 @@
-from lib.audition import Audition
+from audition import Audition
 
+class Role: #the character in the play
 
-class Role:
+    all_roles = []
+
+    def __init__(self, character_name):
+        self.character_name = character_name
+        
+    @property
+    def character_name(self):
+        return self._character_name
+    
+    @character_name.setter
+    def character_name(self, character_name):
+        if  type(character_name) == str and (len(character_name) > 0):
+            self._character_name = character_name
+            Role.all_roles.append(self)
+        else:
+            raise Exception("Character name must be a string greater than 0 characters")
+        
+    def auditions(self):
+        return Audition.all_auditions
     pass
+
+ceclia = Role("Juliet")
+
+print(ceclia.character_name)
+print(Role.all_roles[0])
+
+print(ceclia.auditions())
